f737bf6ebce0b48e4f59c03ade960c21b919e41d
EdA20 <64848449+EdA20@users.noreply.github.com>
2023-03-06 17:24:30 +0300

    Update boosting.py

--- ml_experiments/src/modeling/boosting.py
+++ ml_experiments/src/modeling/boosting.py
@@ -2,29 +2,39 @@ from typing import Tuple
 
 import catboost as cb
 from sklearn.metrics import accuracy_score
+import optuna
+import scipy
+import numpy as np
 
 
 def catboosting(X_train, y_train, X_val, y_val, X_test, y_test, random_state: int = 12) -> Tuple[float, float, float]:
-    params = dict(
-        learning_rate=0.025,
-        iterations=10000,
-        reg_lambda=0.0005,
-        colsample_bylevel=1.,
-        max_bin=80,
-        bagging_temperature=2,
-        use_best_model=True,
-        verbose=False,
-        grow_policy='Depthwise',
-        random_seed=random_state
-    )
-    model = cb.CatBoostClassifier(
-        **params,
-    )
+    params = {
+        'max_depth': optuna.distributions.IntDistribution(1, 12),
+        'learning_rate': optuna.distributions.FloatDistribution(0.01, 1.0, log=True),
+        'n_estimators': optuna.distributions.IntDistribution(50, 1000),
+        'subsample': optuna.distributions.FloatDistribution(0.02, 1.0, log=True),
+        'colsample_bylevel': optuna.distributions.FloatDistribution(0.01, 1.0, log=True),
+        'bagging_temperature': optuna.distributions.FloatDistribution(1e-8, 1.0, log=True),
+        'l2_leaf_reg': optuna.distributions.FloatDistribution(1e-8, 100, log=True),
+        'min_child_samples': optuna.distributions.IntDistribution(1, 10)
+    }
 
-    eval_set = cb.Pool(data=X_val, label=y_val)
-    model.fit(X_train, y_train, eval_set=eval_set, plot=False)
+    model = cb.CatBoostClassifier(verbose=False)
+
+    if scipy.sparse.issparse(X_train):
+        X_train = scipy.sparse.vstack((X_train, X_val))
+        y_train = scipy.sparse.hstack((y_train, y_val))
+    else:
+        X_train = np.vstack((X_train, X_val))
+        y_train = np.hstack((y_train, y_val))
+
+    optuna_search = optuna.integration.OptunaSearchCV(model, params, n_trials=100, random_state=12)
+    optuna_search.fit(X_train, y_train)
+
+    model = cb.CatBoostClassifier(**optuna_search.best_params_)
+    model.fit(X_train, y_train)
+
     train_score = accuracy_score(model.predict(X_train), y_train)
-
     val_score = accuracy_score(model.predict(X_val), y_val)
     test_score = accuracy_score(model.predict(X_test), y_test)
 
